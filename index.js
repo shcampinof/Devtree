f676f6fceec835c0bee0dcba91c957be55eaaf00
S E B A S T I A N C A M P I Ñ O <shcampinof@unal.edu.co>
2025-11-10 05:08:48 -0500

    Creación de servidor en Express

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hola Mundo desde Devtree')
+const express = require('express')
+
+const app = express()
+
+
+//Routing
+
+app.get('/', (req,res)=>{
+    res.send('Hola mundo en Express')
+})
+
+app.get('/ecommerce', (req,res)=>{
+    res.send('Página de Ecommerce')
+})
+
+
+app.listen(4000, ()=>{
+    console.log("Servidor Funcionando...")
+
+})
